43568788497f6c8df3e357feec5064baa08656cf
melisa108 <kagyulife@gmail.com>
2023-02-17 23:33:24 +0100

    add homework addToWishListTest

--- src/test/java/DemowebshopTests.java
+++ src/test/java/DemowebshopTests.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -41,10 +42,10 @@ public class DemowebshopTests {
     @Test
     void addToCartAnonymTest() {
         String body = "product_attribute_72_5_18=65" +
-                        "&product_attribute_72_6_19=54" +
-                        "&product_attribute_72_3_20=57" +
-                        "&product_attribute_72_8_30=94" +
-                        "&addtocart_72.EnteredQuantity=3";
+                "&product_attribute_72_6_19=54" +
+                "&product_attribute_72_3_20=57" +
+                "&product_attribute_72_8_30=94" +
+                "&addtocart_72.EnteredQuantity=3";
 
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -57,5 +58,30 @@ public class DemowebshopTests {
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
                 .body("updatetopcartsectionhtml", is("(3)"));
+    }
+
+    @Test
+    @DisplayName("Authorized customer adds the item in wish list ")
+    void addToWishListTest() {
+        String cookieValue = "CEE9638B0D901722979027B105EC69CAB66F3BBC6002FC4D4DA2F96508DF91B6FECC053" +
+                "7FC37841822B34469E1E8262C3FB257252F9C280265228D598D3A36A1FDAFE64CF87D91B23C7176AF48A" +
+                "73D93F533961EAAC1B23DDB24348A6E3BFAC957DE754A0DBA97467DB761867B59BBCB564D6217929F2E25" +
+                "774319313743360D204726E8B76839E80A5278812B8AC6B19A7998A8D75E6D6A7D7F9DE974BBF3BEEFF5" +
+                "B1BF35FD6FE7558F5B1ADBF316AE",
+                bodyValue = "giftcard_4.RecipientName=Tester&giftcard_4.SenderName=King+Author&" +
+                        "giftcard_4.Message=&addtocart_4.EnteredQuantity=1";
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("NOPCOMMERCE.AUTH", cookieValue)
+                .body(bodyValue)
+                .when()
+                .post("/addproducttocart/details/4/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
+
     }
 }
